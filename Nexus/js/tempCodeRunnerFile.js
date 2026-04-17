fac16f20ec5dd8ec9c0e8dda6ee4df879d2c2117
shubham kumar <shubham31dynamic@gmail.com>
2026-04-17 17:15:46 +0530

    kjdcn

--- Nexus/js/tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-
-// // filter
-// let arr = [10,19,17,19,20,27,26];
-// const result1 = arr.filter((num)=> {
-//     return num%2==0;
-// })
-// console.log(result);
--- Nexus/js/while.js
+++
@@ -1,6 +0,0 @@
-let i =1,n=5;
-while(i<=10){
-    mul=n*i;
-    console.log(mul);
-    i++;
-}
